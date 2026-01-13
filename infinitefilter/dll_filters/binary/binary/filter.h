e2d74d13371b0ebc2aa3c578b5de9546c3c76d25
infgotoinf <infgotoinf@gmail.com>
2026-01-13 15:21:12 +0300

    Delete infinitefilter/dll_filters folder

--- infinitefilter/dll_filters/binary/binary/filter.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef FILTER_EXPORTS
-#define FILTER_API __declspec(dllexport)
-#else
-#define FILTER_API __declspec(dllimport)
-#endif
-
-extern "C" FILTER_API void filter(unsigned char* img, int width, int height, int channels);
